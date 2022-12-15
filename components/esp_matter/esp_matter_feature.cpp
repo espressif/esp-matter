a07469e35f4a8c38a00ff21eb435cc351b76f6e4
Diego Rodríguez Royo <jdrr1998@hotmail.com>
2022-12-15 09:05:50 +0000

    esp_matter_feature: window_covering: absolute_positioning not lift & tilt exclusive

--- components/esp_matter/esp_matter_feature.cpp
+++ components/esp_matter/esp_matter_feature.cpp
@@ -454,36 +454,47 @@ esp_err_t add(cluster_t *cluster, config_t *config)
     }
     update_feature_map(cluster, get_id());
 
-    uint32_t abs_and_pa_lf_feature_map = get_id() | feature::position_aware_lift::get_id();
-    uint32_t abs_and_pa_tl_feature_map = get_id() | feature::position_aware_tilt::get_id();
+    uint32_t abs_and_pa_lf_and_lf_feature_map = get_id() | feature::position_aware_lift::get_id() | feature::lift::get_id();
+    uint32_t abs_and_pa_tl_and_tl_feature_map = get_id() | feature::position_aware_tilt::get_id() | feature::tilt::get_id();
     uint32_t abs_and_lift_feature_map = get_id() | feature::lift::get_id();
     uint32_t abs_and_tilt_feature_map = get_id() | feature::tilt::get_id();
-    if((get_feature_map_value(cluster) & abs_and_pa_lf_feature_map) == abs_and_pa_lf_feature_map) {
+    if (
+        (get_feature_map_value(cluster) & abs_and_pa_lf_and_lf_feature_map) != abs_and_pa_lf_and_lf_feature_map
+        && (get_feature_map_value(cluster) & abs_and_pa_tl_and_tl_feature_map) != abs_and_pa_tl_and_tl_feature_map
+        && (get_feature_map_value(cluster) & abs_and_lift_feature_map) != abs_and_lift_feature_map
+        && (get_feature_map_value(cluster) & abs_and_tilt_feature_map) != abs_and_tilt_feature_map
+    ) {
+        ESP_LOGE(TAG, "Cluster shall support Lift (and optionally Position_Aware_Lift) and/or Tilt (and optionally Position_Aware_Tilt) features");
+        return ESP_ERR_NOT_SUPPORTED;
+    }
+    if((get_feature_map_value(cluster) & abs_and_pa_lf_and_lf_feature_map) == abs_and_pa_lf_and_lf_feature_map) {
         attribute::create_physical_closed_limit_lift(cluster, config->physical_closed_limit_lift);
         attribute::create_current_position_lift(cluster, config->current_position_lift);
         attribute::create_installed_open_limit_lift(cluster, config->installed_open_limit_lift);
         attribute::create_installed_closed_limit_lift(cluster, config->installed_closed_limit_lift);
     } else {
-        ESP_LOGE(TAG, "Cluster shall support Position_Aware_Lift feature");
-        return ESP_ERR_NOT_SUPPORTED;
+        ESP_LOGI(TAG, "Lift related attributes were not created because cluster does not support Position_Aware_Lift feature");
     }
 
-    if((get_feature_map_value(cluster) & abs_and_pa_tl_feature_map) == abs_and_pa_tl_feature_map) {
+    if((get_feature_map_value(cluster) & abs_and_pa_tl_and_tl_feature_map) == abs_and_pa_tl_and_tl_feature_map) {
         attribute::create_physical_closed_limit_tilt(cluster, config->physical_closed_limit_tilt);
         attribute::create_current_position_tilt(cluster, config->current_position_tilt);
         attribute::create_installed_open_limit_tilt(cluster, config->installed_open_limit_tilt);
         attribute::create_installed_closed_limit_tilt(cluster, config->installed_closed_limit_lift);
     } else {
-        ESP_LOGE(TAG, "Cluster shall support Position_Aware_Tilt feature");
-        return ESP_ERR_NOT_SUPPORTED;
+        ESP_LOGI(TAG, "Tilt related attributes were not created because cluster does not support Position_Aware_Tilt feature");
     }
 
     if((get_feature_map_value(cluster) & abs_and_lift_feature_map) == abs_and_lift_feature_map) {
 	    command::create_go_to_lift_value(cluster);
+    } else {
+        ESP_LOGI(TAG, "Lift commands were not created because cluster does not support Lift feature");
     }
 
     if((get_feature_map_value(cluster) & abs_and_tilt_feature_map) == abs_and_tilt_feature_map) {
-    	command::create_go_to_tilt_value(cluster);
+        command::create_go_to_tilt_value(cluster);
+    } else {
+        ESP_LOGI(TAG, "Tilt commands were not created because cluster does not support Tilt feature");
     }
 
     return ESP_OK;
