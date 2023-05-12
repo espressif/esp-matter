4a3f6959bae560395293e24c29dfa8b3090f29b8
Shubham Patil <shubham.patil@espressif.com>
2023-05-12 15:54:37 +0530

    Added esp_matter_mem_realloc API

--- components/esp_matter/esp_matter_mem.cpp
+++ components/esp_matter/esp_matter_mem.cpp
@@ -1,4 +1,4 @@
-// Copyright 2021 Espressif Systems (Shanghai) PTE LTD
+// Copyright 2023 Espressif Systems (Shanghai) PTE LTD
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -26,6 +26,19 @@ IRAM_ATTR void *esp_matter_mem_calloc(size_t n, size_t size)
     return heap_caps_calloc_prefer(n, size, 2, MALLOC_CAP_INTERNAL|MALLOC_CAP_IRAM_8BIT, MALLOC_CAP_INTERNAL|MALLOC_CAP_8BIT);
 #else
     return calloc(n, size);
+#endif
+}
+
+IRAM_ATTR void *esp_matter_mem_realloc(void *ptr, size_t size)
+{
+#if CONFIG_ESP_MATTER_MEM_ALLOC_MODE_INTERNAL
+    return heap_caps_realloc(ptr, size, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#elif CONFIG_ESP_MATTER_MEM_ALLOC_MODE_EXTERNAL
+    return heap_caps_realloc(ptr, size, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+#elif CONFIG_ESP_MATTER_MEM_ALLOC_MODE_IRAM_8BIT
+    return heap_caps_realloc_prefer(ptr, size, 2, MALLOC_CAP_INTERNAL | MALLOC_CAP_IRAM_8BIT, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#else
+    return realloc(ptr, size);
 #endif
 }
 
