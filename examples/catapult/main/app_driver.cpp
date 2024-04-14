4ff8f0e4875931937ef766f9e2c8417400f7d985
cag8xn <158217230+cag8xn@users.noreply.github.com>
2024-04-14 19:43:25 +0200

    Added support for 4 servos

--- examples/catapult/main/app_driver.cpp
+++ examples/catapult/main/app_driver.cpp
@@ -26,32 +26,51 @@ using namespace esp_matter::cluster;
 static const char *TAG = "app_driver";
 extern uint16_t light_endpoint_id;
 
-/*ELIA start*/
 // Define GPIO pin connected to the signal input of the servo
-#define SERVO_GPIO 13
+#define SERVO1_GPIO 13
+#define SERVO2_GPIO 25
+#define SERVO3_GPIO 33
+#define SERVO4_GPIO 32
+
+#define NUM_SERVOS 4 // Define the number of servos
+
+// Define servo names
+const char* servo_names[NUM_SERVOS] = {"Tensioner", "Release", "Lid", "Reload"};
+
+// Define GPIO pins for each servo
+const int servo_pins[NUM_SERVOS] = {SERVO1_GPIO, SERVO2_GPIO, SERVO3_GPIO, SERVO4_GPIO};
+
+// Define two separate timers
+const mcpwm_timer_t servo_pwm_timer[NUM_SERVOS] = {MCPWM_TIMER_0, MCPWM_TIMER_0, MCPWM_TIMER_1, MCPWM_TIMER_1};
+
+// Define MCPWM output signals for each servo
+const mcpwm_io_signals_t servo_pwm_signals[NUM_SERVOS] = {MCPWM0A, MCPWM0B, MCPWM1A, MCPWM1B};
+
+// Define MCPWM generators for each servo
+const mcpwm_generator_t servo_pwm_generator[NUM_SERVOS] = {MCPWM_OPR_A, MCPWM_OPR_B, MCPWM_OPR_A, MCPWM_OPR_B};
+
 
 // Define parameters for the servo motor
 #define SERVO_MIN_PULSEWIDTH 500  // Minimum pulse width in microseconds
 #define SERVO_MAX_PULSEWIDTH 2500 // Maximum pulse width in microseconds
-#define SERVO_MAX_DEGREE 90       // Maximum angle in degrees servo can rotate
+#define SERVO_MAX_DEGREE 180       // Maximum angle in degrees servo can rotate
 
-static void toggle_OnOff_attribute(){
-    uint16_t endpoint_id_e = light_endpoint_id;
-    uint32_t cluster_id_e = OnOff::Id;
-    uint32_t attribute_id_e = OnOff::Attributes::OnOff::Id;
+// ELIA: Following code TOGGLES the OnOff attribute.
+static void toggle_OnOff_attribute() {
+    uint16_t endpoint_id = light_endpoint_id;
+    uint32_t cluster_id = OnOff::Id;
+    uint32_t attribute_id = OnOff::Attributes::OnOff::Id;
 
-    node_t *node_e = node::get();
-    endpoint_t *endpoint_e = endpoint::get(node_e, endpoint_id_e);
-    cluster_t *cluster_e = cluster::get(endpoint_e, cluster_id_e);
-    attribute_t *attribute_e = attribute::get(cluster_e, attribute_id_e);
+    node_t *node = node::get();
+    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
+    cluster_t *cluster = cluster::get(endpoint, cluster_id);
+    attribute_t *attribute = attribute::get(cluster, attribute_id);
 
-    esp_matter_attr_val_t val_e = esp_matter_invalid(NULL);
-    attribute::get_val(attribute_e, &val_e);
-    val_e.val.b = !val_e.val.b;
-    attribute::update(endpoint_id_e, cluster_id_e, attribute_id_e, &val_e);
-
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+    attribute::get_val(attribute, &val);
+    val.val.b = !val.val.b;
+    attribute::update(endpoint_id, cluster_id, attribute_id, &val);
 }
-/*ELIA end*/
 
 /* Do any conversions/remapping for the actual value here */
 /*ELIA MODS*/
@@ -59,9 +78,48 @@ static esp_err_t app_driver_light_set_power(led_indicator_handle_t handle, esp_m
 {
     if (val->val.b) {
         ESP_LOGI(TAG, "================================== Catapult power ON signal received: %d", val->val.b);
+        
+        mcpwm_config_t pwm_config;
+        pwm_config.frequency = 50;  // Set frequency to 50Hz, suitable for most servos
+        pwm_config.cmpr_a = 0;      // Initial duty cycle
+        pwm_config.counter_mode = MCPWM_UP_COUNTER;
+        pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+        mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+        mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);
+
+        // Initialize and configure MCPWM for each servo
+        for (int servo_id = 0; servo_id < NUM_SERVOS; servo_id++) {
+            ESP_LOGI(TAG, "Initializing %s with GPIO pin %d", servo_names[servo_id], servo_pins[servo_id]);
+            mcpwm_gpio_init(MCPWM_UNIT_0, servo_pwm_signals[servo_id], servo_pins[servo_id]);
+            
+        }
+        // Rotate each servo from 0 to 90 degrees
+        for (int i = 0; i <= SERVO_MAX_DEGREE; i++) {
+            for (int servo_id = 0; servo_id < NUM_SERVOS; servo_id++) {
+                mcpwm_set_duty_in_us(MCPWM_UNIT_0, servo_pwm_timer[servo_id], servo_pwm_generator[servo_id], 
+                                        SERVO_MIN_PULSEWIDTH + ((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * i / SERVO_MAX_DEGREE));
+            }
+            vTaskDelay(20 / portTICK_PERIOD_MS); // Wait 20ms between each step
+        }
+
+        // Rotate each servo from 90 to 0 degrees
+        for (int i = SERVO_MAX_DEGREE; i >= 0; i--) {
+            for (int servo_id = 0; servo_id < NUM_SERVOS; servo_id++) {
+                mcpwm_set_duty_in_us(MCPWM_UNIT_0, servo_pwm_timer[servo_id], servo_pwm_generator[servo_id], 
+                                        SERVO_MIN_PULSEWIDTH + ((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * i / SERVO_MAX_DEGREE));
+            }
+            vTaskDelay(20 / portTICK_PERIOD_MS); // Wait 20ms between each step
+        }
+
+        // Stop PWM generation for each servo
+        for (int servo_id = 0; servo_id < NUM_SERVOS; servo_id++) {
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, servo_pwm_timer[servo_id], servo_pwm_generator[servo_id], 0);
+            mcpwm_stop(MCPWM_UNIT_0, servo_pwm_timer[servo_id]);
+        }
+
+        /*
         // Initialize MCPWM
         mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SERVO_GPIO);
-
         // Configure MCPWM
         mcpwm_config_t pwm_config;
         pwm_config.frequency = 50;  // Set frequency to 50Hz, suitable for most servos
@@ -69,29 +127,31 @@ static esp_err_t app_driver_light_set_power(led_indicator_handle_t handle, esp_m
         pwm_config.counter_mode = MCPWM_UP_COUNTER;
         pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
         mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+        
 
-        // Rotate servo from 0 to 90 degrees
+        // Rotate servo from 0 to 180 degrees
         for (int i = 0; i <= SERVO_MAX_DEGREE; i++) {
             mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 
                                     SERVO_MIN_PULSEWIDTH + ((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * i / SERVO_MAX_DEGREE));
             vTaskDelay(20 / portTICK_PERIOD_MS); // Wait 20ms between each step
         }
-        // Rotate servo from 90 to 0 degrees
+        // Rotate servo from 180 to 0 degrees
         for (int i = SERVO_MAX_DEGREE; i >= 0; i--) {
             mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 
                                     SERVO_MIN_PULSEWIDTH + ((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * i / SERVO_MAX_DEGREE));
             vTaskDelay(20 / portTICK_PERIOD_MS); // Wait 20ms between each step
         }
-
+        
         // Set PWM duty cycle to 0
         mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 0);
 
         // Deinitialize MCPWM
         mcpwm_stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
+        */
 
-        ESP_LOGI(TAG, "ELIA ================================== Attempting to turn off the OnOff attribute");
-        // ELIA: Following code TOGGLES the OnOff attribute. It is duplicate of the app_driver_button_toggle_cb with _e to avoid conflicts
-        toggle_OnOff_attribute()
+
+        ESP_LOGI(TAG, "ELIA ================================== Attempting to turn OFF the OnOff attribute");
+        toggle_OnOff_attribute();
     }
     return ESP_OK;
 }
@@ -110,19 +170,7 @@ static esp_err_t app_driver_light_set_brightness(led_indicator_handle_t handle, 
 static void app_driver_button_toggle_cb(void *arg, void *data)
 {
     ESP_LOGI(TAG, "Toggle button pressed");
-    uint16_t endpoint_id = light_endpoint_id;
-    uint32_t cluster_id = OnOff::Id;
-    uint32_t attribute_id = OnOff::Attributes::OnOff::Id;
-
-    node_t *node = node::get();
-    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
-    cluster_t *cluster = cluster::get(endpoint, cluster_id);
-    attribute_t *attribute = attribute::get(cluster, attribute_id);
-
-    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
-    attribute::get_val(attribute, &val);
-    val.val.b = !val.val.b;
-    attribute::update(endpoint_id, cluster_id, attribute_id, &val);
+    toggle_OnOff_attribute();
 }
 
 esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
