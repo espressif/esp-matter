b0800f27c2797d1c06396867c1396debdaf54635
Shubham Patil <shubham.patil@espressif.com>
2024-07-05 15:49:46 +0530

    components/esp_matter: Do not set the groups cluster feature to 1
    Fixes https://github.com/espressif/esp-matter/issues/992

--- components/esp_matter/esp_matter_cluster.cpp
+++ components/esp_matter/esp_matter_cluster.cpp
@@ -1230,7 +1230,7 @@ cluster_t *create(endpoint_t *endpoint, config_t *config, uint8_t flags)
 
     if (flags & CLUSTER_FLAG_SERVER) {
         /* Attributes managed internally */
-        global::attribute::create_feature_map(cluster, 1);
+        global::attribute::create_feature_map(cluster, 0);
 #if CHIP_CONFIG_ENABLE_EVENTLIST_ATTRIBUTE
         global::attribute::create_event_list(cluster, NULL, 0, 0);
 #endif
