6d3283246c12f2d5763f8e3085c4756e3bec5e9c
Shubham Patil <shubham.patil@espressif.com>
2024-08-22 19:30:36 +0530

    examples: Fix the sequence of sdkconfig loading

--- examples/common/cmake_common/components_include.cmake
+++ examples/common/cmake_common/components_include.cmake
@@ -2,14 +2,18 @@ cmake_minimum_required(VERSION 3.5)
 
 message(IDF_VERSION ": $ENV{IDF_VERSION}; idf version number: ${IDF_VERSION_MAJOR}.${IDF_VERSION_MINOR}.${IDF_VERSION_PATCH}")
 get_filename_component(SDKCONFIG_COMMON_DIR "${CMAKE_CURRENT_SOURCE_DIR}/sdkconfig.defaults" ABSOLUTE)
-set(SDKCONFIG_FILES "${SDKCONFIG_COMMON_DIR}")
 
-# checking if sdkconfig.defaults.target_device exists
-get_filename_component(SDKCONFIG_TARGET "${SDKCONFIG_COMMON_DIR}.${IDF_TARGET}" ABSOLUTE)
-if(EXISTS ${SDKCONFIG_TARGET})
-    list(APPEND SDKCONFIG_FILES "${SDKCONFIG_TARGET}")
-endif()
+# Sequence of loading default sdkconfig files are based on
+# https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-guides/build-system.html#custom-sdkconfig-defaults
+#
+# First default file should be sdkconfig.defaults,
+# if that is present then sdkconfig.defaults.target will be auto loaded
+set(SDKCONFIG_FILES "sdkconfig.defaults")
 
+# Append the configurations externally provided using `SDKCONFIG_DEFAULTS` variable
+list(APPEND SDKCONFIG_FILES ${SDKCONFIG_DEFAULTS})
+
+# Then append any C2 specific files
 # checking if IDF_TARGET is esp32c2
 if(IDF_TARGET STREQUAL "esp32c2")
     # checking if sdkconfig.defaults.esp32c2.idf_ver exists
@@ -29,6 +33,7 @@ if(EXISTS ${SDKCONFIG_SHELL_IDF_VER} AND ${IDF_VERSION_MAJOR}.${IDF_VERSION_MINO
     list(APPEND SDKCONFIG_FILES "${SDKCONFIG_SHELL_IDF_VER}")
 endif()
 
-# setting the final sdkconfig path
-message(SDKCONFIG_PATH ": ${SDKCONFIG_FILES}")
-list(APPEND SDKCONFIG_DEFAULTS ${SDKCONFIG_FILES})
+# setting the final sdkconfig files list to SDKCONFIG_DEFAULTS
+set(SDKCONFIG_DEFAULTS ${SDKCONFIG_FILES})
+
+message("Sequence of SDKCONFIG files: ${SDKCONFIG_DEFAULTS}")
