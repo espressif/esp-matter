69abbea527faea79bb1bb5ce476c66349b02a560
Rohit Jadhav <rohit.jadhav@espressif.com>
2024-09-03 09:57:28 +0530

    esp-matter/components: Fix bounds of SystemMode attribute of Thermostat Fixes https://github.com/espressif/esp-matter/issues/1067

--- components/esp_matter/esp_matter_cluster.cpp
+++ components/esp_matter/esp_matter_cluster.cpp
@@ -1500,7 +1500,7 @@ cluster_t *create(endpoint_t *endpoint, config_t *config, uint8_t flags, uint32_
             global::attribute::create_cluster_revision(cluster, config->cluster_revision);
             attribute::create_local_temperature(cluster, config->local_temperature);
             attribute::create_control_sequence_of_operation(cluster, config->control_sequence_of_operation, 0x0, 0x5);
-            attribute::create_system_mode(cluster, config->system_mode, 0x0, 0x7);
+            attribute::create_system_mode(cluster, config->system_mode, 0x0, 0x9);
         } else {
             ESP_LOGE(TAG, "Config is NULL. Cannot add some attributes.");
         }
