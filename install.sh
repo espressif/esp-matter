d350b595bb0473afeeadd3dc350d1c0993aa4831
Shubham Patil <shubham.patil@espressif.com>
2024-10-15 14:22:47 +0530

    setup: Add option to skip sourcing bootstrap script

--- install.sh
+++ install.sh
@@ -6,16 +6,28 @@ set -e
 print_help() {
     echo "Options:"
     echo "  --no-host-tool          Disable installation of host tools"
+    echo "  --no-bootstrap          Disable sourcing connectedhomeip's scripts/bootstrap.sh,"
+    echo "                          This can be helpful if there's already present connectedhomeip setup"
     echo "  --help                  Display this help message"
+}
+
+echo_log() {
+  echo $@
+  echo ""
 }
 
 # Parse command-line arguments
 NO_HOST_TOOL=false
+NO_BOOTSTRAP=false
+
 while [[ "$#" -gt 0 ]]; do
   case $1 in
          --no-host-tool)
-                 NO_HOST_TOOL=true
-                 ;;
+            NO_HOST_TOOL=true
+            ;;
+        --no-bootstrap)
+            NO_BOOTSTRAP=true
+            ;;
         --help)
             print_help
             exit 1
@@ -33,43 +45,40 @@ ESP_MATTER_PATH=$(cd "${basedir}"; pwd)
 MATTER_PATH=${ESP_MATTER_PATH}/connectedhomeip/connectedhomeip
 export ZAP_INSTALL_PATH=${MATTER_PATH}/.environment/cipd/packages/zap
 
-echo ""
-echo "Running Matter Setup"
-echo ""
-cd ${MATTER_PATH}
-source ${MATTER_PATH}/scripts/bootstrap.sh
-cd ${ESP_MATTER_PATH}
 
-echo ""
-echo "Building host tools"
-echo ""
-
-if [ $NO_HOST_TOOL = false ]; then
-  gn --root="${MATTER_PATH}" gen ${MATTER_PATH}/out/host --args='chip_inet_config_enable_ipv4=false'
-  ninja -C ${MATTER_PATH}/out/host chip-cert chip-tool
-  echo ""
-  echo "Host tools built at: ${MATTER_PATH}/out/host"
+if [ $NO_BOOTSTRAP = false ]; then
+  echo_log "Running Matter Setup"
+  cd ${MATTER_PATH}
+  source ${MATTER_PATH}/scripts/bootstrap.sh
+  cd ${ESP_MATTER_PATH}
+else
+  echo_log "Skipping Matter Setup"
 fi
 
-echo ""
-echo "Exit Matter environment"
-echo ""
-deactivate
+if [ $NO_HOST_TOOL = false ]; then
+  echo_log "Building host tools"
+  gn --root="${MATTER_PATH}" gen ${MATTER_PATH}/out/host --args='chip_inet_config_enable_ipv4=false'
+  ninja -C ${MATTER_PATH}/out/host chip-cert chip-tool
+  echo_log "Host tools built at: ${MATTER_PATH}/out/host"
+else
+  echo_log "Skip building host tools"
+fi
 
-echo ""
-echo "Installing python dependencies for Matter"
-echo ""
+if [ $NO_BOOTSTRAP = false ]; then
+  echo_log "Exit Matter environment"
+  deactivate
+fi
+
+echo_log "Installing python dependencies for Matter"
 
 # Install python dependencies based on idf version
 if [[ $(git -C $IDF_PATH describe) == v4.4* ]]; then
-  echo "Installing requirements from requirements_idf_v4.4.txt"
-  python3 -m pip install -r ${ESP_MATTER_PATH}/requirements_idf_v4.4.txt
+  echo_log "Installing requirements from requirements_idf_v4.4.txt"
+  python3 -m pip install -r ${ESP_MATTER_PATH}/requirements_idf_v4.4.txt >/dev/null
 else
-  echo "Installing requirements from requirements.txt"
-  python3 -m pip install -r ${ESP_MATTER_PATH}/requirements.txt
+  echo_log "Installing requirements from requirements.txt"
+  python3 -m pip install -r ${ESP_MATTER_PATH}/requirements.txt > /dev/null
 fi
 
-echo "All done! You can now run:"
-echo ""
-echo "  . ${basedir}/export.sh"
-echo ""
+echo_log "All done! You can now run:"
+echo_log "  . ${basedir}/export.sh"
