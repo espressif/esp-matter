cce19c935b3a3008be1102655975768ac7dd068f
Tomas McGuinness <tomas@tomasmcguinness.com>
2025-12-02 10:46:44 +0000

    Fix parameter order in commissioning callbacks

--- components/esp_matter_controller/commands/esp_matter_controller_pairing_command.cpp
+++ components/esp_matter_controller/commands/esp_matter_controller_pairing_command.cpp
@@ -52,7 +52,7 @@ void pairing_command::OnCommissioningSuccess(chip::PeerId peerId)
     if (m_callbacks.commissioning_success_callback) {
         auto fabric = controller_instance.get_commissioner()->GetFabricTable()->FindFabricWithCompressedId(
             peerId.GetCompressedFabricId());
-        m_callbacks.commissioning_success_callback(ScopedNodeId(fabric->GetFabricIndex(), peerId.GetNodeId()));
+        m_callbacks.commissioning_success_callback(ScopedNodeId(peerId.GetNodeId(), fabric->GetFabricIndex()));
     }
 }
 
@@ -68,7 +68,7 @@ void pairing_command::OnCommissioningFailure(
         auto fabric = controller_instance.get_commissioner()->GetFabricTable()->FindFabricWithCompressedId(
             peerId.GetCompressedFabricId());
         m_callbacks.commissioning_failure_callback(
-            ScopedNodeId(fabric->GetFabricIndex(), peerId.GetNodeId()), error, stageFailed,
+            ScopedNodeId(peerId.GetNodeId(), fabric->GetFabricIndex()), error, stageFailed,
             additionalErrorInfo.HasValue() ? std::make_optional(additionalErrorInfo.Value()) : std::nullopt);
     }
     if (m_device_is_icd) {
